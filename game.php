19be2279cfeb7a43ace9592594dec318a676a099
Josh <2107767@talnet.nl>
2020-01-15 11:42:02 +0100

    Uitwerking van de exercise

--- game.php
+++ game.php
@@ -1,69 +1,65 @@
-<?php
- 
-$enemy[0] = "Steen";
-$enemy[1] = "Papier";
-$enemy[2] = "Schaar";
- 
- 
-$random = rand(0, count($enemy));
- 
-if ($_SERVER['REQUEST_METHOD'] == 'POST'){
- 
-$jij = $_POST['jij'];
- 
-       if ($jij == $enemy[$random]){
-               echo "Het is gelijkspel";
-       }
- 
-       elseif ($jij == 'Steen' && $enemy[2] == 'Schaar'){
-               echo "Jij wint";
-       }
-       elseif ($jij == 'Papier' && $enemy[0] == 'Steen'){
-               echo "Jij wint";
-       }
- 
-       elseif ($jij == 'Schaar' && $enemy[1] == 'Papier'){
-               echo "Jij wint";
-       }
- 
-       elseif ($jij == 'Steen' && $enemy[1] == 'Papier'){
-               echo "Jij verliest";
-       }
- 
- 
- 
-       elseif ($jij == 'Papier' && $enemy[2] == 'Schaar'){
-               echo "Jij verliest";
-       }
- 
-       elseif ($jij == 'Schaar' && $enemy[0] == 'Steen'){
-               echo "Jij verliest";
-       }
- 
- 
-       else {
- 
-               echo "Het is mislukt";
-       }
-}
- 
-       else{
- 
-?>
 <html>
+<form method='GET'>
 
-    <form method='POST' action=''>
+<?php
+if(!isset($_GET['p1'])){
+?>
 
-    <b>Maak je keuze:</b><br>
-    <select name='jij'>
-    <option value='Steen'>Steen</option>
-    <option value='Papier'>Papier</option>
-    <option value='Schaar'>Schaar</option>
+    <b>Maak je keuze player 1:</b><br>
+    <select name='p1'>
+    <option value='steen'>Steen</option>
+    <option value='papier'>Papier</option>
+    <option value='schaar'>Schaar</option>
     </select>
-    <input type='submit' name='submit'>
+    <input type='submit' name='submit1'>
 
-    </form>
+<?php
+}
+
+if(!isset($_GET['p2'])&& isset($_GET['p1'])){
+    ?>
+
+<b>Maak je keuze player 2:</b><br>
+<select name='p2'>
+<option value='steen'>Steen</option>
+<option value='papier'>Papier</option>
+<option value='schaar'>Schaar</option>
+</select>
+<input type='submit' name='submit2'>
+<input type="hidden" name="p1" value="<?php echo $_GET['p1']; ?>">
+
 
 <?php
 }
 ?>
+</form>
+<?php
+
+if (isset($_GET["p1"])){
+    $keuze1 = $_GET["p1"];
+}
+ 
+if (isset($_GET["p2"])){
+    $keuze2 = $_GET["p2"];
+}
+
+if($keuze1 == "steen" && $keuze2 == "papier"  ||
+    $keuze1 == "schaar" && $keuze2 == "steen" ||
+    $keuze1 == "papier" && $keuze2 == "schaar") {
+        echo "player 2 wint";
+    }
+
+if($keuze2 == "steen" && $keuze1 == "papier" ||
+    $keuze2 == "schaar" && $keuze1 == "steen" ||
+    $keuze2 == "papier" && $keuze1 == "schaar") {
+        echo "player 1 wint";
+    }
+
+if ($keuze1 == $keuze2) {
+    echo "gelijkspel";
+}
+?>
+
+// <?php
+// }
+// ?>
